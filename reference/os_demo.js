aa66b90e0b7028192eedb533944fa0107a67f73f
Graham Flaspoehler <grahamflas@gmail.com>
2024-06-18 09:00:13 -0400

    OS module demo

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+import os from "os";
+
+// Platform (Windows, Mac OS, etc)
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home directory
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
